337aec378cd7ce0bb065be9732fc031643b0855f
iamducthanh <65590543+iamducthanh@users.noreply.github.com>
2022-01-22 21:23:33 +0700

    up

---
+++ src/main/java/com/techcam/techcam/TechCamApplication.java
@@ -0,0 +1,13 @@
+package com.techcam.techcam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechCamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechCamApplication.class, args);
+    }
+
+}
