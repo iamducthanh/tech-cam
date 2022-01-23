1a6e9fc2f5e23c34773799a16ce078def5361dbd
XuShiTa <namlun96@gmail.com>
2022-01-23 19:05:29 +0700

    phase 1

---
+++ src/main/java/com/techcam/controller/admin/StaffController.java
@@ -0,0 +1,23 @@
+package com.techcam.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Project_name : SMW_TECHCAM
+ *
+ * @author : XuShiTa
+ * @version : 1.0
+ * @since : 23.1.2022
+ * Description :
+ */
+@RequestMapping("/staff")
+@Controller
+public class StaffController {
+
+    @GetMapping
+    public String index() {
+        return "admin/001_Staff_Management";
+    }
+}
