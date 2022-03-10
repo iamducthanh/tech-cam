6218ca209b90027ccab3ebaf8c03f1c38318a167
quang <quangngoc201197@gamil.com>
2022-03-10 13:47:51 +0700

    change font voucher and product

--- src/main/java/com/techcam/dto/ProductDto.java
+++ src/main/java/com/techcam/dto/ProductDto.java
@@ -7,10 +7,8 @@ import lombok.Setter;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 
 import static com.techcam.util.ConstantsErrorCode.*;
-import static com.techcam.util.ContainsFormat.*;
 
 /**
  * Description :
