fbcb08cb622077362121cc640b430c5df43ddd77
doduy1995 <81599142+doduy1995@users.noreply.github.com>
2022-03-13 15:20:28 +0700

    done Custoemr

--- src/main/java/com/techcam/entity/CustomerEntity.java
+++ src/main/java/com/techcam/entity/CustomerEntity.java
@@ -25,7 +25,6 @@ public class CustomerEntity {
     private String status;
     private Date createDate;
     private String modifierBy;
-    private Boolean deleteFlag;
 
     @Id
     @Column(name = "id")
@@ -117,49 +116,4 @@ public class CustomerEntity {
         this.modifierBy = modifierBy;
     }
 
-    @Basic
-    @Column(name = "delete_flag")
-    public Boolean getDeleteFlag() {
-        return deleteFlag;
-    }
-
-    public void setDeleteFlag(Boolean deleteFlag) {
-        this.deleteFlag = deleteFlag;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        CustomerEntity that = (CustomerEntity) o;
-
-        if (id != null ? !id.equals(that.id) : that.id != null) return false;
-        if (fullName != null ? !fullName.equals(that.fullName) : that.fullName != null) return false;
-        if (phoneNumber != null ? !phoneNumber.equals(that.phoneNumber) : that.phoneNumber != null) return false;
-        if (email != null ? !email.equals(that.email) : that.email != null) return false;
-        if (address != null ? !address.equals(that.address) : that.address != null) return false;
-        if (dateOfBirth != null ? !dateOfBirth.equals(that.dateOfBirth) : that.dateOfBirth != null) return false;
-        if (status != null ? !status.equals(that.status) : that.status != null) return false;
-        if (createDate != null ? !createDate.equals(that.createDate) : that.createDate != null) return false;
-        if (modifierBy != null ? !modifierBy.equals(that.modifierBy) : that.modifierBy != null) return false;
-        if (deleteFlag != null ? !deleteFlag.equals(that.deleteFlag) : that.deleteFlag != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (fullName != null ? fullName.hashCode() : 0);
-        result = 31 * result + (phoneNumber != null ? phoneNumber.hashCode() : 0);
-        result = 31 * result + (email != null ? email.hashCode() : 0);
-        result = 31 * result + (address != null ? address.hashCode() : 0);
-        result = 31 * result + (dateOfBirth != null ? dateOfBirth.hashCode() : 0);
-        result = 31 * result + (status != null ? status.hashCode() : 0);
-        result = 31 * result + (createDate != null ? createDate.hashCode() : 0);
-        result = 31 * result + (modifierBy != null ? modifierBy.hashCode() : 0);
-        result = 31 * result + (deleteFlag != null ? deleteFlag.hashCode() : 0);
-        return result;
-    }
 }
