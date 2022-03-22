65c1b9697b1f5d06c5f4f57a2a4cc3be6067a438
Hoang Huy <huyhoike@gmail.com>
2022-03-22 21:37:24 +0700

    chuc nang quan ly khuyen mai
    chuc nang quan ly khuyen mai

---
+++ src/main/java/com/techcam/service/impl/PromotionProductService.java
@@ -0,0 +1,8 @@
+package com.techcam.service.impl;
+
+import com.techcam.service.IPromotionProductService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PromotionProductService implements IPromotionProductService {
+}
