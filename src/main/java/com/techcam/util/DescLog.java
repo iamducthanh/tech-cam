00ef67f614f8b18eb01a78de4c01deff7dbac125
doduy1995 <81599142+doduy1995@users.noreply.github.com>
2022-03-28 20:21:59 +0700

    done log server

---
+++ src/main/java/com/techcam/util/DescLog.java
@@ -0,0 +1,15 @@
+package com.techcam.util;
+
+/**
+ * Description:
+ *
+ * @author: POLY_DuyDVPH12712
+ * @version: 1.0
+ * @since: 3/28/2022
+ * Project_name: Tech-cam
+ */
+
+public interface DescLog {
+    // log của customer
+    String LOG_INSERT_CUSTOMER = "%s vừa thêm mới khách hàng %s";
+}
