d0b0b344310251fb2d0cb7c8eca39e09e3fb8d27
quang <ngocnq@runsystem.net>
2022-04-24 23:35:04 +0700

    tạo báo cáo, fix một số bug giao diện

--- src/main/java/com/techcam/entity/ProductEntity.java
+++ src/main/java/com/techcam/entity/ProductEntity.java
@@ -1,11 +1,93 @@
 package com.techcam.entity;
 
+import com.techcam.dto.Report.DailySaleDto;
+import com.techcam.dto.Report.StaffSaleDto;
+import com.techcam.dto.Report.TopProductSaleByMonthDto;
+import com.techcam.dto.Report.TopProductSaleDto;
 import lombok.*;
 import org.hibernate.Hibernate;
 
 import javax.persistence.*;
 import java.util.List;
 import java.util.Objects;
+
+
+@NamedNativeQuery(
+        name = "getDailySaleReport",
+        query = "CALL PR_DAILY_SALES_REPORT(?1)",
+        resultSetMapping = "getDailySaleReport"
+)
+@NamedNativeQuery(
+        name = "getStaffSaleReport",
+        query = "CALL PR_STAFF_SALES(?1, ?2)",
+        resultSetMapping = "getStaffSaleReport"
+)
+@NamedNativeQuery(
+        name = "getTopProductSale",
+        query = "CALL PR_TOP_PRODUCT_SALE(?1, ?2, ?3)",
+        resultSetMapping = "getTopProductSale"
+)
+@NamedNativeQuery(
+        name = "getTopProductSaleByMonth",
+        query = "CALL PR_TOP_PRODUCT_SALE_BY_MONTH(?1, ?2, ?3)",
+        resultSetMapping = "getTopProductSaleByMonth"
+)
+
+@SqlResultSetMapping(
+        name = "getDailySaleReport",
+        classes = @ConstructorResult(
+                targetClass = DailySaleDto.class,
+                columns = {
+                        @ColumnResult(name = "PRODUCT_CODE", type = String.class),
+                        @ColumnResult(name = "NAME", type = String.class),
+                        @ColumnResult(name = "price", type = Long.class),
+                        @ColumnResult(name = "total_quantity", type = Integer.class),
+                        @ColumnResult(name = "PROMOTION", type = Long.class),
+                        @ColumnResult(name = "total_amount", type = Long.class)
+                }
+        )
+)
+
+@SqlResultSetMapping(
+        name = "getStaffSaleReport",
+        classes = @ConstructorResult(
+                targetClass = StaffSaleDto.class,
+                columns = {
+                        @ColumnResult(name = "sales_person", type = String.class),
+                        @ColumnResult(name = "FULL_NAME", type = String.class),
+                        @ColumnResult(name = "sumReceiptValue", type = Long.class)
+                }
+        )
+)
+
+@SqlResultSetMapping(
+        name = "getTopProductSale",
+        classes = @ConstructorResult(
+                targetClass = TopProductSaleDto.class,
+                columns = {
+                        @ColumnResult(name = "PRODUCT_CODE", type = String.class),
+                        @ColumnResult(name = "price", type = String.class),
+                        @ColumnResult(name = "total_quantity", type = Integer.class),
+                        @ColumnResult(name = "total_amout", type = Long.class)
+                }
+        )
+)
+
+@SqlResultSetMapping(
+        name = "getTopProductSaleByMonth",
+        classes = @ConstructorResult(
+                targetClass = TopProductSaleByMonthDto.class,
+                columns = {
+                        @ColumnResult(name = "PRODUCT_CODE", type = String.class),
+                        @ColumnResult(name = "NAME", type = String.class),
+                        @ColumnResult(name = "image", type = String.class),
+                        @ColumnResult(name = "price", type = Long.class),
+                        @ColumnResult(name = "total_quantity", type = Integer.class),
+                        @ColumnResult(name = "discount", type = Long.class),
+                        @ColumnResult(name = "total_amout", type = Long.class)
+                }
+        )
+)
 
 /**
  * @author DucBV
---
+++ src/main/java/com/techcam/service/impl/AuthService.java
@@ -0,0 +1,39 @@
+package com.techcam.service.impl;
+
+import com.techcam.entity.StaffEntity;
+import com.techcam.exception.TechCamExp;
+import com.techcam.service.IAuthService;
+import com.techcam.service.IStaffService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+/**
+ * Description :
+ *
+ * @author : quang
+ * @version : 1.0
+ * @since : 2022/04/24 21:49
+ * Project_name : tech-cam
+ */
+
+@Service
+@RequiredArgsConstructor
+public class AuthService implements IAuthService {
+
+    private final IStaffService staffService;
+
+    @Override
+    public StaffEntity getAuth() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (Objects.nonNull(authentication)) {
+            String email = authentication.getName();
+            return staffService.getByEmail(email);
+        }
+        throw new TechCamExp("Bạn không có quyền thao tác");
+    }
+
+}
